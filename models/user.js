aa59baee48722136c9e4f2877a320d430a39c8c2
Junaid Farooq <junaid@evercam.io>
2016-04-23 13:59:32 +0500

    Added Hashing password functionality before saving to DB

--- models/user.js
+++ models/user.js
@@ -21,11 +21,21 @@ var UserSchema = new Schema({
 	}]
 });
 
-
-
 // Hash the password before save it into database
 
+UserSchema.pre("save", function(next){
+	var user = this;
+	if (!user.isModified("password")) return next();
 
+	bcrypt.genSalt(10, function(err, salt){
+		if (err) return next();
+		bcrypt.Hash(user.password, salt, null, function(err, hash){
+			if (err) return next();
+			user.password = hash;
+			next();
+		});
+	});
+});
 
 
 
