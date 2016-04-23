0f19d01c1553d0fac9bea134bae4147d8b1d0444
Junaid Farooq <junaid@evercam.io>
2016-04-23 15:07:05 +0500

    Added Body Parser and testing route for User Schema

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
 var express = require("express"),
-	morgan = require("morgan");
-var	app = express();
+		morgan = require("morgan"),
+		mongoose = require("mongoose"),
+		bodyParser = require("body-parser"),
+		app = express(),
+		User = require("./models/user");
+
+
+// connecting MongoLabs
+
+mongoose.connect("mongodb://root:123456@ds019268.mlab.com:19268/amazonclone", function(err){
+	if (err) {
+		console.log(err);
+	}
+	else{
+		console.log("Connected to the Database");
+	}
+});
 
 // Middleware
 
 app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ entended: true }));
 
-app.get("/home", function(req,res){
-	res.json("hello")
+
+// Testing route for user Schema
+
+app.post("create-user", function(req, res){
+	var user = new User();
+	user.profile.name
 });
+
+// created 3000 port as server
 
 app.listen(3000, function(err){
 	if (err) throw error;
