d2dbe974eb7b15cc251107e6b93927ebe68b22a2
Junaid Farooq <junaid@evercam.io>
2016-04-23 14:09:23 +0500

    Exported Mongoose Model as User

--- models/user.js
+++ models/user.js
@@ -42,3 +42,6 @@ UserSchema.pre("save", function(next){
 UserSchema.methods.comparePassword = function(password) {
 	return bcrypt.compareSync(password, this.password);
 };
+
+
+module.exports = mongoose.model("User", UserSchema);
